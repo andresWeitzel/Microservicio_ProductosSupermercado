05dd1dbacbae12ca79bd8009990dc903c10212dd
Andrés Weitzel <andres96energy@hotmail.com>
2022-08-01 16:30:55 -0300

    Updated Auth

--- src/main/java/com/api/produc/sup/security/dto/JwtDto.java
+++ src/main/java/com/api/produc/sup/security/dto/JwtDto.java
@@ -1,27 +1,15 @@
 package com.api.produc.sup.security.dto;
 
-import java.util.Collection;
-
-import org.springframework.security.core.GrantedAuthority;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@NoArgsConstructor
 @AllArgsConstructor
+@NoArgsConstructor
 public class JwtDTO {
+	
     private String token;
-    private String bearer = "Bearer";
-    private String username;
-    private Collection<? extends GrantedAuthority> authorities;
-
-    public JwtDTO(String token, String username, Collection<? extends GrantedAuthority> authorities) {
-        this.token = token;
-        this.username = username;
-        this.authorities = authorities;
-    }
-
   
 }
